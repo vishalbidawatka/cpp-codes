eadf271f8fcc4fd1f426ef2dd295d0fd0ad28981
SOPNESH <42608037+SOPNESH@users.noreply.github.com>
2019-02-26 20:42:57 +0530

    Update fast_exponential.cpp

--- fast_exponential.cpp
+++ fast_exponential.cpp
@@ -1,50 +1,32 @@
 #include<iostream>
 using namespace std;
 
-
-
 void fast_exp(int base, int power)
 {
 
-long long int result=1;
+    long long int result=1;
+     while(power>0)
+    {
+  	if(power%2==0)
+  	{
+   	power/=2;
+   	base*=base;
+  	} 
 
-
-
- while(power>0)
- {
-  if(power%2==0)
-  {
-   power/=2;
-   base*=base;
-
-  } 
-
-  else
-
-   {
+  	else
+	{
    	result*=base;
    	power-=1;
-   }
-
-
-
-
+    }
 }
 cout<<result;
-
-
 }
-
-
 
 int main()
 {
-
-	int base,power;
-	cin>>base>>power;
-	fast_exp(base,power);
-
-
-return 0;
-
+    int base,power;
+    cin>>base>>power;
+    fast_exp(base,power);
+	
+    return 0;
 }
